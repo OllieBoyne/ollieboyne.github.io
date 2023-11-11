d7e79b265c689d80ec91608df86a389f6784da50
OllieBoyne <48369287+OllieBoyne@users.noreply.github.com>
2023-11-11 16:34:45 +0000

    Deploying to gh-pages from @ OllieBoyne/ollieboyne.github.io@dc29e71f1e9ab678dc803d2638734d6cb8ac1d88 🚀

--- precache-manifest.a2fab8a2cca201ab058046aa9fab2ca0.js
+++ precache-manifest.a2fab8a2cca201ab058046aa9fab2ca0.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "43f7b9f054b9e3fe7e859c0640c0d989",
+    "revision": "bbb5b197488fff9e288cf018a8378e4f",
     "url": "/index.html"
   },
   {
-    "revision": "01cf43b0d2dd431c030d",
-    "url": "/static/css/main.1f9e0e94.chunk.css"
+    "revision": "6729795489cf8c7e880d",
+    "url": "/static/css/main.14864de1.chunk.css"
   },
   {
     "revision": "12e7aa30ea54e4cefb0f",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.0d12708b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "01cf43b0d2dd431c030d",
+    "revision": "6729795489cf8c7e880d",
     "url": "/static/js/main.2bf988f2.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.432c927d6dc2610fdbf8a4120ddc5a29.js"
+  "/precache-manifest.a2fab8a2cca201ab058046aa9fab2ca0.js"
 );
 
 self.addEventListener('message', (event) => {
