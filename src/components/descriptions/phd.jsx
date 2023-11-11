a7dfd4b9b17fa59b65e543c840ac5dddc820b873
Ollie Boyne <48369287+OllieBoyne@users.noreply.github.com>
2023-11-11 16:26:59 +0000

    Minor UI

--- src/components/descriptions/phd.jsx
+++ src/components/descriptions/phd.jsx
@@ -5,8 +5,7 @@ import './descriptions.css';
 export const FoundDescription = () => (
   <div>
     <p><i>FOUND: Foot Optimisation With Uncertain Normals For Surface Deformation Using Synthetic Data</i></p>
-    <p><i>Oliver Boyne, Gwangbin Bae, James Charles, Roberto Cipolla</i></p>
-    <p className="cite-end"><i>WACV 2024</i></p>
+    <p className="cite-end"><i>Oliver Boyne, Gwangbin Bae, James Charles, Roberto Cipolla - WACV 2024</i></p>
     <p>Surface normal prediction (trained from synthetic data), followed by 3D shape reconstruction fusing multiview normals and uncertainty.</p>
     <img src="https://www.ollieboyne.com/FOUND/images/itw/sliders.gif" alt="Surface normal predictions on in-the-wild foot images"/>
   </div>
@@ -22,8 +21,7 @@ export const SynFootDescription = () => (
 export const FindDescription = () => (
     <div>
       <p><i>FIND: An Unsupervised Implicit 3D Model Of Articulated Human Feet</i></p>
-      <p><i>Oliver Boyne, James Charles, Roberto Cipolla</i></p>
-      <p className="cite-end"><i>BMVC 2022</i></p>
+      <p className="cite-end"><i>Oliver Boyne, James Charles, Roberto Cipolla - BMVC 2022</i></p>
       <p>Building a generative model for 3D shape reconstruction.</p>
 
       <iframe title="FIND Video" className="video-frame" width="560" height="315" src="https://www.youtube.com/embed/ehp0z-u7zXg?si=FIjwbIXNUDP1vlyI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
