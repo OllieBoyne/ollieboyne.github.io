b4f0bff0fb2603d7c54f95f5383b74b26f8f9fb8
Ollie Boyne <48369287+OllieBoyne@users.noreply.github.com>
2023-11-13 19:56:41 +0000

    Better contact bar handling

--- src/components/contactBar.jsx
+++ src/components/contactBar.jsx
@@ -1,34 +1,33 @@
 import React from 'react';
-import { FaLinkedin, FaGithub, FaEnvelope, FaFile } from 'react-icons/fa'; // Importing icons
 import './contactBar.css';
 
 const ContactBar = () => (
   <div className="contactbar-container">
-    <div className="headshot-name-icons-container">
-      <img className="headshot" src="assets/sidebar/headshot-2 icon.jpg" alt="Headshot" />
+    <img className="headshot" src="assets/sidebar/headshot-2 icon.jpg" alt="Headshot" />
+    <div className="headshot-name-container">
+
+    <div className="info-container">
       <div className="name-phd-location-container">
-        <div className="name">Ollie Boyne</div>
-        <div className="phd-info">PhD Student</div>
-        <div className="location-info">Cambridge, UK</div>
+          <div className="name">Ollie Boyne</div>
+          <div className="phd-info">PhD Student</div>
+          <div className="location-info">Cambridge, UK</div>
+        </div>
       </div>
-      <div className="contact-icon-grid">
-        <a className="contact-icon cv-icon" href="/assets/cv/cv_23.pdf" download="OBoyne_CV.pdf">
-          <div className="contact-icon-overlay">
-            <FaFile />
-            <span className="contact-icon-text">CV</span>
-          </div>
-        </a>
-        <a className="contact-icon linkedin-icon" href="https://www.linkedin.com/in/OllieBoyne">
-          <FaLinkedin />
-        </a>
-        <a className="contact-icon email-icon" href="mailto:ollieboyne@gmail.com">
-          <FaEnvelope />
-        </a>
-        <a className="contact-icon github-icon" href="https://github.com/OllieBoyne">
-          <FaGithub />
-        </a>
+      <div className="contact-icon-row">
+          <a className="contact-icon cv-icon" href="/assets/cv/cv_23.pdf" download="OBoyne_CV.pdf">
+            <img src="assets/sidebar/icons/cv.png" alt="Download CV"/>
+          </a>
+          <a className="contact-icon email-icon" href="mailto:ollieboyne@gmail.com">
+            <img src="assets/sidebar/icons/email.png" alt="Email"/>
+          </a>
+          <a className="contact-icon github-icon" href="https://github.com/OllieBoyne">
+            <img src="assets/sidebar/icons/github.png" alt="GitHub Profile"/>
+          </a>
+          <a className="contact-icon linkedin-icon" href="https://www.linkedin.com/in/OllieBoyne">
+            <img src="assets/sidebar/icons/linkedin.png" alt="LinkedIn"/>
+          </a>
+        </div>
       </div>
-    </div>
   </div>
 );
 
